aae954f54ab56bb37ea94b9abc8a9884010cbd0e
Thane Thomson <connect@thanethomson.com>
2021-12-01 22:43:08 -0500

    Fix integration tests
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- loiter/tests/integration.rs
+++ loiter/tests/integration.rs
@@ -15,21 +15,15 @@ fn store_and_load() {
     let loaded_project1 = store.project("project-1").unwrap();
     assert_eq!(project1, loaded_project1);
 
-    let task1 = Task::new(
-        "project-1",
-        "Some task",
-        config.task_state_config().initial(),
-    );
+    let task1 =
+        Task::new("project-1", "Some task").with_state(config.task_state_config().initial());
     let task1 = store.save_task(&task1).unwrap();
     assert_eq!(task1.id().unwrap(), 1);
     let loaded_task1 = store.task("project-1", 1).unwrap();
     assert_eq!(task1, loaded_task1);
 
-    let task2 = Task::new(
-        "project-1",
-        "Another task",
-        config.task_state_config().done(),
-    );
+    let task2 =
+        Task::new("project-1", "Another task").with_state(config.task_state_config().done());
     let task2 = store.save_task(&task2).unwrap();
     assert_eq!(task2.id().unwrap(), 2);
     let loaded_task2 = store.task("project-1", 2).unwrap();
@@ -44,11 +38,8 @@ fn rename_project() {
 
     let project1 = Project::new("Project 1");
     store.save_project(&project1).unwrap();
-    let task1 = Task::new(
-        "project-1",
-        "Some task",
-        config.task_state_config().initial(),
-    );
+    let task1 =
+        Task::new("project-1", "Some task").with_state(config.task_state_config().initial());
     let task1 = store.save_task(&task1).unwrap();
 
     let project_renamed = project1.with_name("Project renamed");
